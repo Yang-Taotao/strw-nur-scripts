fcf60aa4efb443b14d8511bc9638789f07fc94c6
T. Yang <yangtaotao0524@gmail.com>
2026-02-16 19:46:51 +0100

    feat: partial solve for tutorial_1+2 and tutorial_3 - feat: solve q1 q2 on `tutorial_1+2` - feat: add media images needed for tutorial 1+2 and 3 - feat: solve q1 on `tutorial_3`

---
+++ src/nur_a/tutorial_1+2.py
@@ -0,0 +1,94 @@
+"""
+Scripts for tutorial session 1+2
+"""
+
+# %%
+# imports
+import numpy as np
+import math
+import timeit
+from matplotlib.image import imread
+
+# %% question 1
+# q1 - func
+def sinc_x_lib(x):
+    """np implementation of sinc"""
+    return np.sin(x) / x
+
+
+def sinc_x_power(x, n):
+    """ps implementation of sinc"""
+    n_ary = np.arange(0, n, 1)
+    top = np.sum((-1) ** n * x ** (2 * n))
+    bot = math.factorial(2 * n + 1)
+    return top / bot
+
+
+def numerical_errors(x, n):
+    """compare np sinc and ps sinc"""
+    return sinc_x_power(x, n) - sinc_x_lib(x)
+
+
+def compare_errors(x: int = 7, n_range: int = 10):
+    """do comparison with multiple choices of n"""
+    result = [numerical_errors(x, n_arg) for n_arg in range(1, n_range + 1)]
+    return result
+
+# %%
+# q1 - func call
+q1_result = compare_errors()
+print(q1_result)
+
+# %% question 2
+# q2 - const assignment
+G = 6.67 * 10 ** (-11)
+C = 3 * 10 ** (8)
+C_INV = 1 / C
+C_INV2 = C_INV**2
+
+# %%
+# q2 - func
+def bh_gen(mean: int = 1000000, stdev: int = 100000, size: int = 10000):
+    """create `size` number of bh from gaussian distribution"""
+    return np.random.normal(loc=mean, scale=stdev, size=size)
+
+
+def rs_div(mass: np.ndarray):
+    """directly calculate Schwarzschild radius rs with dividing c**2"""
+    return 2 * G * mass / C**2
+
+
+def rs_mult(mass: np.ndarray):
+    """directly calculate Schwarzschild radius rs with multiplying (1/c)**2"""
+    return 2 * G * mass * C_INV2
+
+
+def rs_compare(mean: int = 1000000, stdev: int = 100000, size: int = 10000):
+    """compare rs comp time with timeit"""
+    masses = bh_gen()
+    t_div = %timeit for mass in masses: rs_div(mass)
+    t_mult = %timeit for mass in masses: rs_mult(mass)
+
+
+# %%
+# q2 - func call
+rs_compare()
+
+# %% question 3
+# q3 - func
+def img_interpolation():
+
+def linear_interpolator(x1, x2):
+    l_lim = 0
+    r_lim = len(x2) - 1
+    while r_lim - l_lim > 1:
+        mid = (l_lim+r_lim) // 2
+        if x2[mid] < x1:
+            l_lim = mid
+        else:
+            r_lim = mid
+    return l_lim, r_lim
+
+def polynomial_interpolator():
+
+
---
+++ src/nur_a/tutorial_3.py
@@ -0,0 +1,98 @@
+"""
+Scripts for tutorial session 3
+"""
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+# %% question 1
+# q1 - image load
+IMG = mpimg.imread("../../media/dog_image.jpg")
+GREY_IMG = np.dot(IMG[..., :3], [0.640, 0.595, 0.155])  # Rec601 luminanc
+
+
+# %%
+# q1 - func
+def img_svd(img=GREY_IMG):
+    """svd image compression"""
+    mat_u, mat_w, mat_vt = np.linalg.svd(GREY_IMG, full_matrices=False)
+    return mat_u, mat_w, mat_vt
+
+
+def img_svd_reconstitute(img=GREY_IMG, n=0):
+    """reconstitute from mats"""
+    mat_u, mat_w, mat_vt = img_svd(img)
+    # recon via np.dot(mat_u, np.dot(mat_w_zeros, mat_vh))
+    if n == 0:
+        mat_w_zeros = np.zeros((img.shape[0], img.shape[1]))
+        np.fill_diagonal(mat_w_zeros, mat_w)
+    else:
+        mat_u = mat_u[:, :n]
+        mat_w = mat_w[:n]
+        mat_vt = mat_vt[:n, :]
+
+    img_recon = mat_u @ np.diag(mat_w) @ mat_vt
+
+    return img_recon
+
+
+def img_svd_reconstitute_elements(img=GREY_IMG, ns: list = [5, 10, 50]):
+    """reconstitute from the first n elements of the mats"""
+    mat_u, mat_w, mat_vt = img_svd(img)
+    img_size = img.size
+    img_recon = []
+    compression = 0
+
+    ns_custom = np.arange(0, 400, 10)
+
+    for n in ns_custom:
+        # find the depth of elements needed to get a good compression ratio
+        if compression < 1.00:
+            # first n elements of the mats
+            mat_u_n = mat_u[:, :n]
+            mat_w_n = mat_w[:n]
+            mat_vt_n = mat_vt[:n, :]
+
+            img_recon_n = mat_u_n @ np.diag(mat_w_n) @ mat_vt_n
+
+            img_recon_n_size = mat_u_n.size + n + mat_vt_n.size
+
+            img_recon.append(img_recon_n)
+
+            compression = img_recon_n_size / img_size
+            depth = n
+
+    print(f"Compression rate {compression} at {depth} elements.")
+
+    return img_recon
+
+
+def img_svd_plot(img=GREY_IMG):
+    """wrapper for img svd plot gen"""
+    img_recon = img_svd_reconstitute(img)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+    # source img
+    ax[0].imshow(img, cmap="gray")
+    ax[0].set_title("Source img")
+    ax[0].axis("off")
+
+    # reconstituted img
+    ax[1].imshow(img_recon, cmap="gray")
+    ax[1].set_title("Reconstituted img")
+    ax[1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+
+# %%
+# q1 - func call
+img_svd_reconstitute_elements()
+img_svd_plot()
+# %% question 2
